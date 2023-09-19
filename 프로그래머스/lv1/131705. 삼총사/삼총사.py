925a2237d1ff49a0a45dbe39991e66cae446bb6e
Ga Dong Sik <62535229+Nesquitto@users.noreply.github.com>
2023-09-19 12:07:27 +0900

    [level 1] Title: 삼총사, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.py
@@ -0,0 +1,8 @@
+from itertools import combinations
+
+def solution(number):
+    answer = 0
+    for i in list(combinations(number, 3)):
+        if sum(i) == 0:
+            answer += 1
+    return answer
